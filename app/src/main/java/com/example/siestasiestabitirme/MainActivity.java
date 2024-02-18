b639e5db1e347ece4e543667d53eb14aa39192fe
nehirenu <83532452+nehirenu@users.noreply.github.com>
2024-02-18 19:44:38 +0300

    nehir git try.

--- app/src/main/java/com/example/siestasiestabitirme/MainActivity.java
+++ app/src/main/java/com/example/siestasiestabitirme/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 // github denemesi
     //github denemesi 2
     // github denemesi 3
-
+    //git denemesi nehir
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
